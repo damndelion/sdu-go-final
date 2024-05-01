272b24c644da3dbe64be37a624a4245cb77de059
Adilet <119230294+siweirdb@users.noreply.github.com>
2024-05-01 22:25:29 +0500

    belong To

--- internal/entity/order.go
+++ internal/entity/order.go
@@ -5,6 +5,7 @@ import "time"
 type Order struct {
 	ID          string    `gorm:"primaryKey"`
 	UserID      string    `gorm:"not null"`
+	User        User      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Status      string    `gorm:"not null"`
 	TotalPrice  int       `gorm:"not null"`
 	Timestamp   time.Time `gorm:"not null"`
--- internal/entity/orderMenu.go
+++ internal/entity/orderMenu.go
@@ -3,5 +3,7 @@ package entity
 type OrderMenu struct {
 	ID      string `gorm:"primaryKey"`
 	OrderID string `gorm:"not null"`
+	Order   Order  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	MenuID  string `gorm:"not null"`
+	Menu    Menu   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
